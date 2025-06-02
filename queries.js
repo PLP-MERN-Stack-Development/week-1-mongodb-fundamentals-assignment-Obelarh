aa75d73acca9dc64fa9a9e0fc10e0db6bcac8a64
Obelarh <obelasunday@gmail.com>
2025-06-02 20:42:53 +0100

    final commit

--- queries.js
+++ queries.js
@@ -94,5 +94,5 @@ db.books.createIndex({ title: 1 })
 // - Create a compound index on `author` and `published_year`
 db.books.createIndex({ author: 1, published_year: 1 })
 
-- Use the `explain()` method to demonstrate the performance improvement with your indexes
+// - Use the `explain()` method to demonstrate the performance improvement with your indexes
 db.books.find({ title: "1984" }).explain("executionStats")
